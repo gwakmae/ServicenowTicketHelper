e9d534747ff33a596e53d76786f383a97063503a
Sungjae Kwak <piagwak@gmail.com>
2025-08-22 21:35:48 +0900

    초기코드: WPF ServiceNow 티켓 헬퍼 프로젝트 구조 설정
    - 기본 WPF 프로젝트 구조 생성
    - MVVM 패턴을 위한 폴더 구조 설정 (ViewModels, Views, Models, Services, Commands)
    - Microsoft.Playwright 패키지 참조 추가
    - .NET 8.0 WPF 프로젝트로 구성

---
+++ ServiceNowTicketHelper/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ServiceNowTicketHelper
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
